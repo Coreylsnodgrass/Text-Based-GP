a7ddfafdf4013dba3dd2c34f485a2ba50b872932
Coreylsnodgrass <101301707+Coreylsnodgrass@users.noreply.github.com>
2022-04-16 17:36:46 -0400

    Create Enemies.java

---
+++ Enemies.java
@@ -0,0 +1,44 @@
+public class Enemies {
+    private int Health;
+    private int Size;
+    private int Damage;
+    private int Movement;
+    private int Speed;
+    private String[] Moveset;
+    private boolean bossStatus;
+
+
+    public void getHealth(){
+
+}
+
+public void setHealth(){
+
+}
+
+public void setSpeed(){
+
+}
+
+public void receiveDamage(int Damage){
+
+}
+
+public boolean dealDamage(){
+ return false;
+}
+
+public String getMove(){
+return "word";
+}
+
+public int bossAttrib(){
+return 0;
+}
+
+public int movementPattern(){
+return 0;
+}
+
+
+}
